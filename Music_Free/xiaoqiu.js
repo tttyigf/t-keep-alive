be42ca6ff0d092d48d89da1cbdc5e7041e503557
Huibq <huibq520@gmail.com>
2024-05-01 13:46:40 +0800

    up

--- Music_Free/xiaoqiu.js
+++ Music_Free/xiaoqiu.js
@@ -456,7 +456,7 @@ module.exports = {
     platform: "小秋音乐",
     author: "Huibq",
     version: "0.2.0",
-    srcUrl: "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Huibq/keep-alive/master/Music_Free/xiaoqiu.js",
+    srcUrl: "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaoqiu.js",
     cacheControl: "no-cache",
     hints: {
         importMusicSheet: [
